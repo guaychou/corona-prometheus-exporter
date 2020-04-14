a5aacc87a2ff5254b14202574116bbd78e0d8acf
Kevin Jonathan Harnanta <kevin.harnanta@gmail.com>
2020-04-14 12:51:59 +0700

    intial release

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/guaychou/learn-prometheus
+
+go 1.13
+
+require (
+	github.com/guaychou/corona-api v1.0.2
+	github.com/prometheus/client_golang v1.5.1
+	github.com/sirupsen/logrus v1.4.2
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+	"time"
+
+	cga "github.com/guaychou/corona-api"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	countryPtr := flag.String("country", "", "Country name you want to get COVID19 status")
+	portPtr := flag.String("listen.port",":10198", "Port listen address")
+	flag.Parse()
+	if *countryPtr=="" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	var (
+		confirmed = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "confirmed_corona_"+*countryPtr,
+			Help: "Current total confirmed corona in "+*countryPtr,
+		})
+		recovered = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "recovered_corona_"+*countryPtr,
+			Help: "Current total recovered corona in "+*countryPtr,
+		})
+		recoveryRate =prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "recovery_rate_corona_"+*countryPtr,
+			Help: "Current recovery rate in "+*countryPtr,
+		})
+		death = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "death_corona_"+*countryPtr,
+			Help: "Current total death people in "+*countryPtr,
+		})
+		deathRate =prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "death_rate_corona_"+*countryPtr,
+			Help: "Current fatality rate in "+*countryPtr,
+		})
+	)
+	prometheus.MustRegister(confirmed)
+	prometheus.MustRegister(recovered)
+	prometheus.MustRegister(death)
+	prometheus.MustRegister(deathRate)
+	prometheus.MustRegister(recoveryRate)
+	go func() {
+		log.Info("Scrapping corona status in "+*countryPtr)
+		for {
+			result:=get(countryPtr)
+			setPrometheusValue(confirmed, recovered, death,deathRate,recoveryRate, result)
+			time.Sleep(5 * time.Second)
+		}
+	}()
+	http.Handle("/metrics", promhttp.Handler())
+	log.Println("Web Server started. Listening on port "+*portPtr)
+	log.Fatal(http.ListenAndServe(*portPtr, nil))
+}
+
+func get(country *string)cga.CurrentCoronaStatus{
+		result:=cga.GetCorona(*country)
+		log.Info("Scrapped . . . ")
+		return result
+}
+
+func setPrometheusValue(confirmed prometheus.Gauge, recovered prometheus.Gauge, death prometheus.Gauge,deathRate prometheus.Gauge,recoveryRate prometheus.Gauge, result cga.CurrentCoronaStatus){
+	confirmed.Set(float64(result.Confirmed.Value))
+	recovered.Set(float64(result.Recovered.Value))
+	death.Set(float64(result.Deaths.Value))
+	deathRate.Set(result.CaseFatalityRate)
+	recoveryRate.Set(result.CaseRecoveryRate)
+}
